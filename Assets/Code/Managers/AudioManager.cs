e543e4357ef572358960d11da23102710bcec812
HennaKristal <hennakristal@outlook.com>
2025-11-17 16:17:32 +0200

    Improved audio manager

--- Assets/Code/Managers/AudioManager.cs
+++ Assets/Code/Managers/AudioManager.cs
@@ -8,11 +8,17 @@ public class AudioManager : MonoBehaviour
     public static AudioManager Instance => _instance;
 
     [SerializeField] private AudioMixer audioMixer;
-    [SerializeField] private Slider MusicVolumeSlider;
-    [SerializeField] private Slider AmbientVolumeSlider;
-    [SerializeField] private Slider SFXVolumeSlider;
-    [SerializeField] private Slider DialogueVolumeSlider;
-    [SerializeField] private Slider UIVolumeSlider;
+    private Slider MusicVolumeSlider;
+    private Slider AmbientVolumeSlider;
+    private Slider SFXVolumeSlider;
+    private Slider DialogueVolumeSlider;
+    private Slider UIVolumeSlider;
+    private TextMeshProUGUI MusicVolumeSliderText;
+    private TextMeshProUGUI AmbientVolumeSliderText;
+    private TextMeshProUGUI SFXVolumeSliderText;
+    private TextMeshProUGUI DialogueVolumeText;
+    private TextMeshProUGUI UIVolumeSliderText;
+    private bool isInitializing = true;
 
 
     private void Awake()
@@ -29,34 +35,64 @@ public class AudioManager : MonoBehaviour
 
     private void Start()
     {
-        LoadVolume("MusicVolume", MusicVolumeSlider);
-        LoadVolume("AmbientVolume", AmbientVolumeSlider);
-        LoadVolume("SFXVolume", SFXVolumeSlider);
-        LoadVolume("DialogueVolume", DialogueVolumeSlider);
-        LoadVolume("UIVolume", UIVolumeSlider);
+        GameObject audioPanel = GameObject.Find("AudioOptions");
+
+        MusicVolumeSlider = audioPanel.transform.Find("MusicVolumeSlider").GetComponent<Slider>();
+        MusicVolumeSliderText = MusicVolumeSlider.transform.Find("ValueLabel").GetComponent<TextMeshProUGUI>();
+
+        AmbientVolumeSlider = audioPanel.transform.Find("AmbientVolumeSlider").GetComponent<Slider>();
+        AmbientVolumeSliderText = AmbientVolumeSlider.transform.Find("ValueLabel").GetComponent<TextMeshProUGUI>();
+
+        SFXVolumeSlider = audioPanel.transform.Find("SFXVolumeSlider").GetComponent<Slider>();
+        SFXVolumeSliderText = SFXVolumeSlider.transform.Find("ValueLabel").GetComponent<TextMeshProUGUI>();
+
+        DialogueVolumeSlider = audioPanel.transform.Find("DialogueVolumeSlider").GetComponent<Slider>();
+        DialogueVolumeText = DialogueVolumeSlider.transform.Find("ValueLabel").GetComponent<TextMeshProUGUI>();
+
+        UIVolumeSlider = audioPanel.transform.Find("UIVolumeSlider").GetComponent<Slider>();
+        UIVolumeSliderText = UIVolumeSlider.transform.Find("ValueLabel").GetComponent<TextMeshProUGUI>();
+
+        LoadVolume("MusicVolume", MusicVolumeSlider, MusicVolumeText);
+        LoadVolume("AmbientVolume", AmbientVolumeSlider, AmbientVolumeText);
+        LoadVolume("SFXVolume", SFXVolumeSlider, SFXVolumeText);
+        LoadVolume("DialogueVolume", DialogueVolumeSlider, DialogueVolumeText);
+        LoadVolume("UIVolume", UIVolumeSlider, UIVolumeText);
+
+        isInitializing = false;
     }
 
     public void OnVolumeSliderChanged()
     {
-        ApplyVolume("MusicVolume", MusicVolumeSlider);
-        ApplyVolume("AmbientVolume", AmbientVolumeSlider);
-        ApplyVolume("SFXVolume", SFXVolumeSlider);
-        ApplyVolume("DialogueVolume", DialogueVolumeSlider);
-        ApplyVolume("UIVolume", UIVolumeSlider);
+        if (isInitializing)
+        {
+            return;
+        }
+
+        ApplyVolume("MusicVolume", MusicVolumeSlider, MusicVolumeText);
+        ApplyVolume("AmbientVolume", AmbientVolumeSlider, AmbientVolumeText);
+        ApplyVolume("SFXVolume", SFXVolumeSlider, SFXVolumeText);
+        ApplyVolume("DialogueVolume", DialogueVolumeSlider, DialogueVolumeText);
+        ApplyVolume("UIVolume", UIVolumeSlider, UIVolumeText);
     }
 
-    private void LoadVolume(string parameter, Slider slider)
+    private void LoadVolume(string parameter, Slider slider, TextMeshProUGUI label)
     {
-        float value = PlayerPrefs.GetFloat(parameter, 1f);
+        float value = PlayerPrefs.GetFloat(parameter, 0.8f);
         slider.value = value;
+        label.text = Mathf.Ceil(value * 100f).ToString() + "%";
         audioMixer.SetFloat(parameter, LinearToDecibel(value));
     }
 
-    private void ApplyVolume(string parameter, Slider slider)
+    private void ApplyVolume(string parameter, Slider slider, TextMeshProUGUI label)
     {
         float value = slider.value;
+        label.text = Mathf.Ceil(value * 100f).ToString() + "%";
         PlayerPrefs.SetFloat(parameter, value);
         audioMixer.SetFloat(parameter, LinearToDecibel(value));
+    }
+
+    public void SaveAudioSettings()
+    {
         PlayerPrefs.Save();
     }
 
