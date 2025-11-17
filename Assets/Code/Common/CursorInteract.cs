7a66932f732aac79cd03fc3a1b10c436997e31b1
HennaKristal <hennakristal@outlook.com>
2025-11-17 04:56:21 +0200

    Improved CursorInteract to detech UI elements

--- Assets/Code/Common/CursorInteract.cs
+++ Assets/Code/Common/CursorInteract.cs
@@ -1,9 +1,9 @@
 using UnityEngine;
+using UnityEngine.EventSystems;
 
-public class CursorInteract : MonoBehaviour
+public class CursorInteract : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
 {
     [SerializeField] private CursorManager.CursorType cursorType;
-
 
     private void OnMouseEnter()
     {
@@ -11,6 +11,16 @@ public class CursorInteract : MonoBehaviour
     }
 
     private void OnMouseExit()
+    {
+        CursorManager.Instance.SetDefaultCursorType();
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        CursorManager.Instance.SetAciveCursorType(cursorType);
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
     {
         CursorManager.Instance.SetDefaultCursorType();
     }
