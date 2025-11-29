788b5946d76a2fc78f04f80bb854a7fa14757eb9
HennaKristal <hennakristal@outlook.com>
2025-11-29 05:02:37 +0200

    Updated Music Manager

--- Assets/Code/Managers/MusicManager.cs
+++ Assets/Code/Managers/MusicManager.cs
@@ -1,15 +1,14 @@
 using System.Collections;
 using UnityEngine;
 
-
 [System.Serializable]
-public struct SoundTrack
+public class SoundTrack
 {
-    public string name;
-    public AudioClip clip;
+    public string songID;
+    public AudioClip audioClip;
+    public bool isLooping;
     [Range(0, 1)] public float volume;
 }
-
 
 [RequireComponent(typeof(AudioSource))]
 public class MusicManager : MonoBehaviour
@@ -19,7 +18,7 @@ public class MusicManager : MonoBehaviour
 
     [SerializeField] private SoundTrack[] soundTracks;
     private AudioSource audioSource;
-    private string currentlyPlaying = "";
+    private string currentlyPlayingID = "";
 
 
     private void Awake()
@@ -35,25 +34,32 @@ public class MusicManager : MonoBehaviour
 
         audioSource = GetComponent<AudioSource>();
 
+        // Mute audio source if there are no preset audio clip
         if (audioSource.clip == null)
         {
             audioSource.volume = 0;
         }
     }
 
-    public void PlayMusic(string trackName, float fadeDuration = 2f, float musicVolume = -1f)
+    public void PlayMusic(string songID, bool? shouldLoop = null, float songFadeDuration = 2f, float songVolume = -1f)
     {
-        AudioClip newTrack = GetClipFromName(trackName);
+        SoundTrack newSong = GetSoundTrackFromID(songID);
+        bool songIsLooping = newSong.isLooping;
 
-        if (musicVolume == -1f)
+        if (songVolume == -1f)
         {
-            musicVolume = GetVolumeFromName(trackName);
+            songVolume = newSong.volume;
         }
 
-        if (newTrack != null)
+        if (shouldLoop != null)
         {
-            currentlyPlaying = trackName;
-            StartCoroutine(AnimateMusicCrossfade(newTrack, musicVolume, fadeDuration));
+            songIsLooping = (bool)shouldLoop;
+        }
+
+        if (newSong.audioClip != null)
+        {
+            currentlyPlayingID = songName;
+            StartCoroutine(AnimateMusicCrossfade(newSong.audioClip, songIsLooping, songFadeDuration, songVolume));
         }
         else
         {
@@ -66,16 +72,17 @@ public class MusicManager : MonoBehaviour
         StartCoroutine(AnimateMusicFadeOut(fadeDuration));
     }
 
-    private IEnumerator AnimateMusicCrossfade(AudioClip nextTrack, float musicVolume, float fadeDuration)
+    private IEnumerator AnimateMusicCrossfade(AudioClip newAudioClip, bool songIsLooping, float songFadeDuration, float songVolume)
     {
         if (audioSource.isPlaying)
         {
-            yield return StartCoroutine(FadeMusicVolume(audioSource.volume, 0, fadeDuration));
+            yield return StartCoroutine(FadeMusicVolume(audioSource.volume, 0, songFadeDuration));
         }
 
-        audioSource.clip = nextTrack;
+        audioSource.clip = newAudioClip;
+        audioSource.loop = songIsLooping;
         audioSource.Play();
-        yield return StartCoroutine(FadeMusicVolume(0, musicVolume, fadeDuration));
+        yield return StartCoroutine(FadeMusicVolume(0, songVolume, songFadeDuration));
     }
 
     private IEnumerator AnimateMusicFadeOut(float fadeDuration)
@@ -87,39 +94,29 @@ public class MusicManager : MonoBehaviour
     private IEnumerator FadeMusicVolume(float startVolume, float endVolume, float fadeDuration)
     {
         float elapsed = 0f;
+
         while (elapsed < fadeDuration)
         {
             elapsed += Time.deltaTime;
-            float percent = Mathf.Clamp01(elapsed / fadeDuration);
-            audioSource.volume = Mathf.Lerp(startVolume, endVolume, percent);
+            float progress = Mathf.Clamp01(elapsed / fadeDuration);
+            audioSource.volume = Mathf.Lerp(startVolume, endVolume, progress);
             yield return null;
         }
+
         audioSource.volume = endVolume;
     }
 
-    private AudioClip GetClipFromName(string name)
+    private SoundTrack GetSoundTrackFromID(string songID)
     {
         foreach (SoundTrack soundTrack in soundTracks)
         {
-            if (soundTrack.name == name)
+            if (soundTrack.songID == songID)
             {
-                return soundTrack.clip;
+                return soundTrack;
             }
         }
 
+        Debug.LogWarning($"Tried to play song with name {name}, but the song was not found.");
         return null;
-    }
-
-    private float GetVolumeFromName(string name)
-    {
-        foreach (SoundTrack soundTrack in soundTracks)
-        {
-            if (soundTrack.name == name)
-            {
-                return soundTrack.volume;
-            }
-        }
-
-        return 1f;
     }
 }
